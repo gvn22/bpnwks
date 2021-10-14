52614911128376457041f0036f590768ce8551e5
Girish Nivarti <nvgirishs@gmail.com>
2021-10-14 10:55:10 +0100

    Changes to method dispatch in ZonalFlow
    These changes are now reflected in the
    methods called here.

--- networks.jl
+++ networks.jl
@@ -1,15 +1,14 @@
 using Revise
 using ZonalFlow
 
-lx = Float64(2π);
-ly = Float64(2π);
-nx = 8;
-ny = 8;
-
 dn = "data/"
+domain  = Domain(extent=(2π,2π),res=(8,8));
+coeffs  = Coefficients(Ω=5.0,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=false);
+forcing = Stochastic(kf=5,dk=2,ε=0.0); # set 0 forcing
+prob    = BetaPlane(domain,coeffs,forcing);
 
-dumpadjacency(lx,ly,nx,ny;fs=dn*"adjacency_nl_8x8",Λ=nx-1)
-dumpadjacency(lx,ly,nx,ny;fs=dn*"adjacency_gql3_8x8",Λ=3)
+dumpadjacency(prob,NL();fn=dn*"adjacency_nl_8x8_test")
+dumpadjacency(prob,GQL(3);fn=dn*"adjacency_gql3_8x8")
 
 # dt = 0.005;
 # t_end = 1000.0;
